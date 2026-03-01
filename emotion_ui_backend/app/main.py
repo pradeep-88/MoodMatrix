e9a79a7570dd601b9b9a3b991982ba37ecbbeffe
Pradeep <121634788+pradeep-88@users.noreply.github.com>
2026-03-01 20:03:03 +0530

    Add new CORS origin for deployment

--- emotion_ui_backend/app/main.py
+++ emotion_ui_backend/app/main.py
@@ -5,7 +5,7 @@ from app.routes import video_emotion, text_sentiment
 app = FastAPI(title="Emotion-Driven UI 🎭")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],    allow_credentials=True,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://mood-matrix-vert.vercel.app/"],    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -16,3 +16,4 @@ app.include_router(text_sentiment.router, prefix="/text")
 @app.get("/")
 def root():
     return {"msg": "Emotion Driven UI Backend Running 🚀"}
+
